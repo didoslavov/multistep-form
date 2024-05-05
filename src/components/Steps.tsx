a806631c560f7bfd3f65affe6412c505efc85edc
didoslavov <didkoslawow@gmail.com>
2024-05-05 03:34:22 +0300

    add validation

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,28 +1,48 @@
+"use client";
+
+import { GlobalState, useStateMachine } from "little-state-machine";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 function Steps() {
+  const path = usePathname();
+  const [state, setState] = useState<GlobalState>();
+  const { getState } = useStateMachine();
+
+  useEffect(() => {
+    setState(getState());
+  }, []);
+
   return (
     <div className="col-start-3 col-end-7 mx-auto mb-20 flex items-center justify-between gap-10 pt-10 text-base text-neutral-400 dark:text-gray-400">
-      <Link href="/">
+      <Link href={state?.firstName && state.lastName && state.email ? "/" : ""}>
         <div className="flex items-center">
-          <div className="flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-xl text-neutral-900">
+          <div
+            className={`flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-xl text-neutral-900 ${path === "/" && "border-0 bg-neutral-500 text-neutral-300"}`}
+          >
             1
           </div>
         </div>
       </Link>
       <div className="h-[2px] w-28 bg-neutral-500"></div>
-      <Link href="/step-2">
+      <Link href={state?.town && state.address && state.phone ? "/step-2" : ""}>
         <div className="flex items-center">
-          <div className="flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-xl text-neutral-900">
+          <div
+            className={`flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-xl text-neutral-900 ${path === "/step-2" && "border-0 bg-neutral-500 text-neutral-300"}`}
+          >
             2
           </div>
         </div>
       </Link>
       <div className="h-[2px] w-28 bg-neutral-500"></div>
-      <Link href="/step-3">
+      <Link
+        href={state?.billing && state.company && state.crn ? "/step-3" : ""}
+      >
         <div className="flex items-center">
-          <div className="text-x border-neutral-900l flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-neutral-900">
+          <div
+            className={`text-x border-neutral-900l flex h-12 w-12 items-center justify-center rounded-full border border-neutral-900 bg-neutral-300 text-neutral-900 ${path === "/step-3" && "border-0 bg-neutral-500 text-neutral-300"}`}
+          >
             3
           </div>
         </div>

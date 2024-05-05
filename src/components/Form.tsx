2625c74682f1e948d70badef048d8e105fad5006
didoslavov <didkoslawow@gmail.com>
2024-05-05 03:34:42 +0300

    add state persistant

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -13,13 +13,48 @@ import { Input } from "./ui/input";
 import { FormProvider, useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useStateMachine } from "little-state-machine";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { updateStep1Data } from "@/lib/updateActions";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  firstName: z.string().min(3),
+  lastName: z.string().min(3),
+  email: z.string().min(3).email(),
+});
 
 function Form() {
-  const form = useForm();
+  const router = useRouter();
+  const { state, actions } = useStateMachine({
+    updateStep1Data,
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      firstName: state.firstName,
+      lastName: state.lastName,
+      email: state.email,
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (formData: z.infer<typeof formSchema>) => {
+    actions.updateStep1Data({
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+    });
+
+    router.push("/step-2");
+  };
 
   return (
     <FormProvider {...form}>
-      <div className="flex flex-col items-center justify-center gap-10 text-neutral-900">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col items-center justify-center gap-10 text-neutral-900"
+      >
         <FormField
           control={form.control}
           name="firstName"
@@ -34,7 +69,7 @@ function Form() {
                 />
               </FormControl>
               <FormDescription>Your first name.</FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-900" />
             </FormItem>
           )}
         />
@@ -52,7 +87,7 @@ function Form() {
                 />
               </FormControl>
               <FormDescription>Your last name.</FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-900" />
             </FormItem>
           )}
         />
@@ -70,19 +105,18 @@ function Form() {
                 />
               </FormControl>
               <FormDescription>This is your email address.</FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-900" />
             </FormItem>
           )}
         />
-        <Link href="/step-2">
-          <Button
-            variant={"outline"}
-            className="border-neutral-900 px-20 py-6 text-lg font-bold hover:bg-neutral-400"
-          >
-            Next
-          </Button>
-        </Link>
-      </div>
+        <Button
+          type="submit"
+          variant={"outline"}
+          className="border-neutral-900 px-20 py-6 text-lg font-bold hover:bg-neutral-400"
+        >
+          Next
+        </Button>
+      </form>
     </FormProvider>
   );
 }
